860503af4f2f211f95803f3b41c5cd5376ccb5ea
salih <salih.abdelkarim1@gmail.com>
2024-11-20 00:20:39 +0100

    programa comlpeta

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,42 @@
+import java.util.Scanner;
+
 public class Calculadora {
     public static void main(String[] args) {
-        
-        System.out.println("holaa ");
+        Scanner sc=new Scanner(System.in);
+        int opc;
+        int primeroNumero,segundoNumero;
+        System.out.println("**************Operaciones**************");
+        System.out.println("1) Suma ");
+        System.out.println("2) Resta ");
+        System.out.println("3) Multiplicacion ");
+        System.out.println("4) Division ");
+        System.out.println("Por favor, entre el numero de la operación desea calcular");
+        opc = sc.nextInt();
+        System.out.println("Entre el primer numero");
+        primeroNumero=sc.nextInt();
+        System.out.println("Entre el segundo numero");
+        segundoNumero=sc.nextInt();
+        switch(opc)
+        {
+            case 1:
+                System.out.println("Resulta es " + (primeroNumero + segundoNumero));
+                break;
+            case 2:
+            System.out.println("Resulta es " + (primeroNumero - segundoNumero));
+                break;
+            case 3:
+            System.out.println("Resulta es " + (primeroNumero * segundoNumero));
+                break;
+            case 4:
+                try {
+                    System.out.println("Resulta es " + (primeroNumero / segundoNumero));
+                } catch (ArithmeticException e) {
+                    System.out.println("El segundo numero no puede ser 0");
+                }
+            
+                break;
+            default:
+                System.out.println("La operación no existe");
+        }
     }
 }
